db7783e50580daad70028e87d88a25e9dfd51e49
tavonbue <theo.vonbueren@hslu.ch>
2024-10-22 07:48:08 +0200

    Projekteröffnung. Erstellen der main.py-Datei.

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+# Pfad zur Datei
+file_path = (r"T:\ta\60 FuE\6033 CC GH+IB\603330 Projekte\02 Forschung\1123118-00 HILTI_holzbau_schubversuche\04 "
+             r"Leistung\versuchsdaten\02_ST01_01\hilti_timbertesting_ST01_01_messdaten.xlsx")
+
+# Excel-Datei einlesen, erste Zeile überspringen, erste Spalte als Index festlegen und leere Zeilen entfernen
+df_raw = pd.read_excel(file_path, skiprows=1, index_col=0).dropna(how='all', axis=0)
+
+# Einheiten auslesen
+units = df_raw.iloc[0, :]
+
+# Funktion, um zu überprüfen, ob eine Zelle numerisch ist
+def is_numeric(val):
+    try:
+        pd.to_numeric(val)
+        return True
+    except ValueError:
+        return False
+
+# Die map-Methode auf jede Spalte anwenden und nur Zeilen behalten, die numerische Werte enthalten
+df = df_raw[df_raw.apply(lambda row: row.map(is_numeric).all(), axis=1)]
+
+# Erste Zeilen der bereinigten Datei anzeigen
+print(df.head())
+
+# Plotten der ersten Spalte
+df.w_01.plot()
+
