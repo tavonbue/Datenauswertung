d94431a8da3ea12b7a4d3aedcb78d1a36c05efc4
tavonbue <theo.vonbueren@hslu.ch>
2024-10-22 15:49:15 +0200

    Neu ist nur noch die Plot-Funktion hier.

--- functions.py
+++ functions.py
@@ -1,33 +1,22 @@
-# Funktion, um zu überprüfen, ob eine Zelle numerisch ist
-def is_numeric(val):
-    import pandas as pd
-    try:
-        pd.to_numeric(val)
-        return True
-    except ValueError:
-        return False
-
-def plot_function(df, x_columns, y, title, x_label, y_label):
-    import matplotlib
+def plot_data(df, units, x_col, y_col):
+    # Plot-Funktion für flexible Achsen
     import matplotlib.pyplot as plt
-    matplotlib.use('TkAgg')
+    from io import BytesIO
+    import base64
+    import matplotlib
 
-    # Erstellen einer neuen Figur und Achsen
-    plt.figure(figsize=(15, 8), dpi=100)
-
-    # Schleife über alle Verformungsspalten und die entsprechenden Kurven plotten
-    for col in x_columns:
-        # Plotten der Verformung auf der X-Achse und der Kraft Q_sup auf der Y-Achse
-        plt.plot(df[col].clip(lower=0), y, label=f'{col} vs. {y.name}')
-
-    # Titel und Achsenbeschriftungen
-    plt.title(title)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-
-    # Legende anzeigen
-    plt.legend()
-
-    # Diagramm anzeigen
+    matplotlib.use('Agg')  # Setzt das nicht-interaktive Backend für das Plotten
+    plt.figure(figsize=(10, 7))
+    plt.plot(df[x_col], df[y_col], label=f'{x_col} vs. {y_col}', color='blue')
+    plt.title(f'{x_col} vs. {y_col}')
+    plt.xlabel(f'{x_col} [{units.loc[x_col]}]')
+    plt.ylabel(f'{y_col} [{units.loc[y_col]}]')
     plt.grid(True)
-    plt.show()
+
+    # Bild in Bytes konvertieren und Base64 kodieren
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    plt.close()
+    return plot_url
