c12bb3f4b994d84950b9a07168ac549e620ba2d1
tavonbue <theo.vonbueren@hslu.ch>
2024-11-06 13:56:47 +0100

    Komplette Überarbeitung der Skripts. Neue Aufteilung in pre- und postprocessing auf zwei html-Skripts.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, send_file
-from plotting import plot_data
+from plotting import plot_data, plotly_interactive_plot
 from import_data import load_file_paths, load_dataframe
-
 from io import BytesIO
-import pickle
-from datetime import datetime
+import pandas as pd
+import os
+
 app = Flask(__name__)
 
 # Pfad zu den Dateien und Ordnern
@@ -20,26 +20,96 @@ for file_path, file_name in zip(excel_file_paths, excel_file_names):
     datasets[file_name] = df
     units_data[file_name] = units
 
-# Hilfsfunktion, um Eingabewerte als float oder None zu verarbeiten
+# Hilfsfunktion, um Eingabewerte als Datum oder None zu verarbeiten
+def parse_date(value):
+    try:
+        return pd.to_datetime(value)
+    except (TypeError, ValueError):
+        return None
+
+# Preprocessing-Seite zum Bearbeiten und Beschneiden des DataFrames
+@app.route('/', methods=['GET', 'POST'])
+def preprocessing():
+    selected_file = request.args.get('file', excel_file_names[0])
+
+    # Prüfen, ob ein bearbeiteter DataFrame existiert
+    processed_file_path = f'processed_{selected_file}.pkl'
+    if os.path.exists(processed_file_path):
+        df = pd.read_pickle(processed_file_path)
+    else:
+        df = datasets[selected_file]  # Original-DataFrame laden
+
+    units = units_data[selected_file]
+
+    # X-Achse immer die erste Spalte (Sekunden), Y-Achse wird ausgewählt
+    x_col = df.columns[0]  # Die X-Achse bleibt immer die erste Spalte
+    y_col = request.args.get('y_col', df.columns[1])
+
+    # Konvertiere die Werte der X-Achse (erste Spalte) in float, falls sie als 'object' formatiert sind
+    try:
+        df[x_col] = df[x_col].astype(float)
+    except ValueError as e:
+        print(f"Fehler bei der Konvertierung von {x_col} zu float: {e}")
+
+    # Überprüfe, ob der Reset-Button gedrückt wurde
+    reset_x_axis = request.args.get('reset_x_axis')
+    if reset_x_axis:
+        # Lade den ursprünglichen DataFrame ohne Bearbeitung
+        df_processed = datasets[selected_file]
+        x_max_input = None  # Setze x_max_input auf None
+    else:
+        # Maximalwert für die X-Achse (Sekunden) aus dem Eingabefeld
+        x_max_input = request.args.get('x_max')
+        df_processed = df
+
+        if x_max_input and x_max_input.lower() != 'none':
+            try:
+                # Versuche, die Eingabe als float zu konvertieren
+                x_max = float(x_max_input)
+                # Beschneide den DataFrame basierend auf der X-Achse (Sekunden)
+                df_processed = df[df[x_col] <= x_max]
+            except ValueError as e:
+                print(f"Fehler bei der Konvertierung von x_max: {e}")
+        else:
+            # Wenn x_max_input nicht gesetzt oder 'None' ist, verwende den ursprünglichen DataFrame
+            df_processed = df
+
+    # Speichere den bearbeiteten DataFrame als Pickle-Datei
+    df_processed.to_pickle(processed_file_path)
+
+    # Erstelle den interaktiven Plot mit dem bearbeiteten DataFrame
+    plot_html = plotly_interactive_plot(
+        df_processed, units, x_col, y_col
+    )
+
+    return render_template('preprocessing.html', files=excel_file_names, selected_file=selected_file,
+                           x_col=x_col, y_col=y_col, plot_html=plot_html, columns=df.columns,
+                           x_max=x_max_input)
+
+
+# Hilfsfunktion, um Eingabewerte als Datum oder None zu verarbeiten
 def parse_float(value):
     try:
         return float(value)
     except (TypeError, ValueError):
         return None
 
-# Hauptseite für die Excel-Datei-, X- und Y-Achsen-Auswahl
-@app.route('/', methods=['GET', 'POST'])
-def select_plot():
-    # Excel-Datei-Auswahl: Standardmäßig wird die erste Datei ausgewählt
+
+# Hauptseite für die Excel-Datei-, X- und Y-Achsen-Auswahl (Post-Processing)
+@app.route('/postprocessing', methods=['GET', 'POST'])
+def postprocessing():
     selected_file = request.args.get('file', excel_file_names[0])
 
-    # DataFrame und Units aus dem vorgeladenen Dictionary abrufen
-    df = datasets[selected_file]
+    # Versuche, den verarbeiteten DataFrame zu laden, ansonsten den Originalen verwenden
+    try:
+        df = pd.read_pickle(f'processed_{selected_file}.pkl')
+    except FileNotFoundError:
+        df = datasets[selected_file]
     units = units_data[selected_file]
 
-    # X- und Y-Achsen Auswahl: Standardmäßig die ersten beiden Spalten
-    x_col = request.args.get('x_col', df.keys()[0])
-    y_col = request.args.get('y_col', df.keys()[1])
+    # X- und Y-Achsen Auswahl
+    x_col = request.args.get('x_col', df.columns[0])
+    y_col = request.args.get('y_col', df.columns[1])
 
     # Minimal- und Maximalwerte sowie Schrittweiten für X- und Y-Achsen
     x_min = parse_float(request.args.get('x_min'))
@@ -56,39 +126,46 @@ def select_plot():
         x_step=x_step, y_step=y_step
     )
 
-    # HTML-Template rendern
-    return render_template('select_plot.html', files=excel_file_names, selected_file=selected_file,
-                           x_col=x_col, y_col=y_col, plot_url=plot_url, columns=df.keys(),
+    return render_template('postprocessing.html', files=excel_file_names, selected_file=selected_file,
+                           x_col=x_col, y_col=y_col, plot_url=plot_url, columns=df.columns,
                            x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max,
                            x_step=x_step, y_step=y_step)
+
+
 
 # Route für den PDF-Download
 @app.route('/download_pdf', methods=['POST'])
 def download_pdf():
-    # Formulardaten abrufen
-    selected_file = request.form.get('file')
-    x_col = request.form.get('x_col')
-    y_col = request.form.get('y_col')
+    selected_file = request.form.get('file', excel_file_names[0])
+
+    try:
+        df = pd.read_pickle(f'processed_{selected_file}.pkl')
+    except FileNotFoundError:
+        df = datasets[selected_file]
+    units = units_data[selected_file]
+
+    # X- und Y-Achsen Auswahl
+    x_col = request.form.get('x_col', df.columns[0])
+    y_col = request.form.get('y_col', df.columns[1])
+
+    # Minimal- und Maximalwerte sowie Schrittweiten für X- und Y-Achsen
     x_min = parse_float(request.form.get('x_min'))
     x_max = parse_float(request.form.get('x_max'))
     y_min = parse_float(request.form.get('y_min'))
     y_max = parse_float(request.form.get('y_max'))
-    x_step = parse_float(request.form.get('x_step'))  # Schrittweite X abrufen
-    y_step = parse_float(request.form.get('y_step'))  # Schrittweite Y abrufen
+    x_step = parse_float(request.form.get('x_step'))
+    y_step = parse_float(request.form.get('y_step'))
 
     # Daten abrufen und den Plot als PDF erstellen
-    df = datasets[selected_file]
-    units = units_data[selected_file]
-
-    pdf_bytes = plot_data(df, units, x_col, y_col, x_min, x_max, y_min, y_max, x_step, y_step, output_format='pdf')
+    pdf_bytes = plot_data(
+        df, units, x_col, y_col, x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max,
+        x_step=x_step, y_step=y_step, output_format='pdf'
+    )
 
     # Dynamischer Dateiname basierend auf der Auswahl
-    pdf_filename = f"{selected_file.split('.')[0]}_{x_col}_vs_{y_col}.pdf"
+    pdf_filename = f"{selected_file.split('.')[0]}_{y_col}.pdf"
 
-    # Sendet die PDF-Datei an den Benutzer
     return send_file(pdf_bytes, as_attachment=True, download_name=pdf_filename, mimetype='application/pdf')
-
-
 
 
 if __name__ == '__main__':
--- plotting.py
+++ plotting.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import matplotlib
-import mplcursors
 from matplotlib.ticker import AutoMinorLocator, MultipleLocator
 from io import BytesIO
 import base64
@@ -31,8 +30,8 @@ def plot_data(df, units, x_col, y_col, x_min=None, x_max=None, y_min=None, y_max
 
     # Plot erstellen
     plt.plot(df[x_col], df[y_col], label=f'{x_col} vs. {y_col}', color='black', linewidth=1)
-    plt.xlabel(f'{x_col} [{units.loc[x_col]}]', loc='right', labelpad=5)
-    plt.ylabel(f'{y_col} [{units.loc[y_col]}]', rotation=0, labelpad=-55, y=1.03, ha='right')
+    plt.xlabel(f'{x_col} [{units.get(x_col, "")}]', loc='right', labelpad=5)
+    plt.ylabel(f'{y_col} [{units.get(y_col, "")}]', rotation=0, labelpad=-55, y=1.03, ha='right')
 
     # Horizontale und vertikale Linie bei y=0 und x=0
     plt.axhline(0, color='black', linestyle='-', linewidth=0.7)
@@ -72,3 +71,77 @@ def plot_data(df, units, x_col, y_col, x_min=None, x_max=None, y_min=None, y_max
         plot_url = base64.b64encode(img.getvalue()).decode('utf8')
         plt.close()
         return plot_url
+
+
+import plotly.graph_objs as go
+
+
+def plotly_interactive_plot(df, units, x_col, y_col, x_min=None, x_max=None, y_min=None, y_max=None, x_step=None,
+                            y_step=None, selected_points=None):
+
+    # Erstelle das Plotly-Objekt
+    fig = go.Figure()
+
+    # Linie und Marker hinzufügen
+    fig.add_trace(go.Scatter(
+        x=df[x_col],  # Setzt die x-Werte auf die ausgewählte Spalte
+        y=df[y_col],  # Setzt die y-Werte auf die ausgewählte Spalte
+        mode='lines',  # Zeigt eine Linie mit Markern (Punkte) an 'lines+markers', 'lines'
+        line=dict(color='black', width=1.5),  # Legt die Linienfarbe auf Schwarz und die Linienstärke auf 1 fest
+        marker=dict(size=4),  # Legt die Größe der Marker fest (hier auf 6 gesetzt)
+        name=f'{x_col} vs. {y_col}'  # Legt den Namen der Linie für die Legende fest
+    ))
+
+    # Markierte Punkte hinzufügen
+    if selected_points:
+        fig.add_trace(go.Scatter(
+            x=[point['x'] for point in selected_points],
+            y=[point['y'] for point in selected_points],
+            mode='markers',
+            marker=dict(size=8, color='red'),  # Markierte Punkte in Rot
+            name='Selected Points'
+        ))
+
+    # Achsenlabels mit Einheiten wie in plot_data
+    fig.update_layout(
+        xaxis_title=f"{x_col} [{units.get(x_col, '')}]",  # Setzt den Titel der x-Achse mit Einheit aus dem 'units'-Dictionary
+        yaxis_title=f"{y_col} [{units.get(y_col, '')}]",  # Setzt den Titel der y-Achse mit Einheit aus dem 'units'-Dictionary
+        font=dict(family="Computer Modern, serif", size=14),  # Legt die Schriftart und -größe fest
+        # title=dict(text=f'{x_col} vs. {y_col}', x=0.5),  # Setzt den Titel des Plots und zentriert ihn horizontal
+        margin=dict(l=50, r=50, t=50, b=50),  # Legt die Abstände zu den Rändern (links, rechts, oben, unten) fest
+        template="plotly_white",  # Setzt das Plotly-Template auf Weiß für einen klaren Hintergrund
+        hovermode="closest",  # Aktiviert interaktives Hovering bei den Datenpunkten
+        plot_bgcolor="rgba(0,0,0,0)",  # Setzt den Plot-Hintergrund auf transparent (kein Hintergrund)
+    )
+
+    # Füge horizontale und vertikale Linie bei x=0 und y=0 hinzu
+    fig.add_shape(type="line", x0=0, x1=0, y0=df[y_col].min(), y1=df[y_col].max(),
+                  line=dict(color="black", width=0.7), layer="below")
+    fig.add_shape(type="line", y0=0, y1=0, x0=df[x_col].min(), x1=df[x_col].max(),
+                  line=dict(color="black", width=0.7), layer="below")
+
+    # Festlegung der Achsengrenzen falls angegeben
+    if x_min is not None and x_max is not None:
+        fig.update_xaxes(range=[x_min, x_max])
+    if y_min is not None and y_max is not None:
+        fig.update_yaxes(range=[y_min, y_max])
+
+    # Schrittweite und Teilstriche wie in plot_data
+    if x_step:
+        fig.update_xaxes(dtick=x_step, tick0=x_min)
+    if y_step:
+        fig.update_yaxes(dtick=y_step, tick0=y_min)
+
+    # Zusätzliche Minor Ticks zwischen den Major Ticks
+    fig.update_xaxes(showgrid=True, minor=dict(ticks="inside", showgrid=True))
+    fig.update_yaxes(showgrid=True, minor=dict(ticks="inside", showgrid=True))
+
+    # Rückgabe als HTML
+    return fig.to_html(full_html=False)
+
+
+
+
+
+
+
